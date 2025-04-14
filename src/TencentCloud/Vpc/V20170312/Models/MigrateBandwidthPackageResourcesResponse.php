d33f6a6008ca9e8a7ee529b7ebf332ec7dda3cf8
tencentcloudapi <tencentcloudapi@tencent.com>
2025-04-15 05:34:21 +0800

    release 3.0.1360

--- src/TencentCloud/Vpc/V20170312/Models/MigrateBandwidthPackageResourcesResponse.php
+++ src/TencentCloud/Vpc/V20170312/Models/MigrateBandwidthPackageResourcesResponse.php
@@ -18,27 +18,19 @@ namespace TencentCloud\Vpc\V20170312\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * DescribeBandwidthPackageBandwidthRange返回参数结构体
+ * MigrateBandwidthPackageResources返回参数结构体
  *
- * @method array getBandwidthRangeSet() 获取带宽包带宽上下限详细信息。ResourceId：带宽包id、BandwidthLowerLimit：带宽下限、BandwidthUpperLimit：带宽上限。
- * @method void setBandwidthRangeSet(array $BandwidthRangeSet) 设置带宽包带宽上下限详细信息。ResourceId：带宽包id、BandwidthLowerLimit：带宽下限、BandwidthUpperLimit：带宽上限。
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
-class DescribeBandwidthPackageBandwidthRangeResponse extends AbstractModel
+class MigrateBandwidthPackageResourcesResponse extends AbstractModel
 {
-    /**
-     * @var array 带宽包带宽上下限详细信息。ResourceId：带宽包id、BandwidthLowerLimit：带宽下限、BandwidthUpperLimit：带宽上限。
-     */
-    public $BandwidthRangeSet;
-
     /**
      * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
-     * @param array $BandwidthRangeSet 带宽包带宽上下限详细信息。ResourceId：带宽包id、BandwidthLowerLimit：带宽下限、BandwidthUpperLimit：带宽上限。
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -54,15 +46,6 @@ class DescribeBandwidthPackageBandwidthRangeResponse extends AbstractModel
         if ($param === null) {
             return;
         }
-        if (array_key_exists("BandwidthRangeSet",$param) and $param["BandwidthRangeSet"] !== null) {
-            $this->BandwidthRangeSet = [];
-            foreach ($param["BandwidthRangeSet"] as $key => $value){
-                $obj = new BandwidthRange();
-                $obj->deserialize($value);
-                array_push($this->BandwidthRangeSet, $obj);
-            }
-        }
-
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
             $this->RequestId = $param["RequestId"];
         }
